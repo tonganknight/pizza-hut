100c7a470ae13a3e6ab638228180f3b741017d3c
Malachi <malachialusa@gmail.com>
2021-03-17 14:29:38 -0600

    added reply to comments, and updated means to view them throughout build

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,6 +1,31 @@
 const { Comment, Pizza } = require('../models');
 
 const commentController = {
+    addReply({ params, body }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true }
+    )
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  },
+  // remove reply
+removeReply({ params }, res) {
+  Comment.findOneAndUpdate(
+    { _id: params.commentId },
+    { $pull: { replies: { replyId: params.replyId } } },
+    { new: true }
+  )
+    .then(dbPizzaData => res.json(dbPizzaData))
+    .catch(err => res.json(err));
+},
     // add comment to pizza
     addComment({ params, body }, res) {
         console.log(body);
